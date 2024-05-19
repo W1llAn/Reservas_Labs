da2a10e00d7dc43fd46bece74ee279a14547ac67
TicheKiwar <kiwartiche@gmail.com>
2024-05-19 11:49:24 -0500

    Metodos clase Lab

--- Reservas_Labs/src/Controladores/Lab.java
+++ Reservas_Labs/src/Controladores/Lab.java
@@ -10,4 +10,114 @@ package Controladores;
  */
 class Lab {
     
+    private int id;
+    private String name;
+    private boolean type;
+    private String code;
+    private int floor;
+    private int idBlock;
+
+    private Lab(int id, String name, boolean type, String code, int floor, int idBlock) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.code = code;
+        this.floor = floor;
+        this.idBlock = idBlock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getIdBlock() {
+        return idBlock;
+    }
+
+    public void setIdBlock(int idBlock) {
+        this.idBlock = idBlock;
+    }
+    
+    public static class LabBuilder{
+     
+    private int id;
+    private String name;
+    private boolean type;
+    private String code;
+    private int floor;
+    private int idBlock;
+
+        public LabBuilder Id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public LabBuilder Name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public LabBuilder Type(boolean type) {
+            this.type = type;
+            return this;
+        }
+
+        public LabBuilder Code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        public LabBuilder Floor(int floor) {
+            this.floor = floor;
+            return this;
+        }
+
+        public LabBuilder IdBlock(int idBlock) {
+            this.idBlock = idBlock;
+            return this;
+        }
+    
+    public Lab build(){
+        return new Lab(id, name, type, code, floor, idBlock);
+    }
+    
+}
+    
+    
 }
