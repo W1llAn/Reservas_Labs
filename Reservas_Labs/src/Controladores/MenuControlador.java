99b192d0363d0dd948a137ed33d162340e1cba02
JaviF <fiallosedisonreggae@gmail.com>
2024-05-19 11:41:26 -0500

    Se agrego las fuentes y colores a usar en el archivo MenuControlador

--- Reservas_Labs/src/Controladores/MenuControlador.java
+++ Reservas_Labs/src/Controladores/MenuControlador.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controladores;
 
-/**
- *
- * @author PC
- */
-public class MenuControlador {
+import Vista.Menu;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class MenuControlador implements MouseListener{
+
+    private Menu menu = new Menu();
+    private final Color rojoOscuro = new Color(147,10,13);
+    private final Color rojoClaro = new Color(216,17,45);
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+    
     
 }
