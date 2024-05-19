283f78658db263ecb996f8ebcc1f5c5fb6ae18ab
W1llAn <wchimborazo2840@uta.edu.ec>
2024-05-19 00:21:41 -0500

    Se crean las conexiones a la base en el modelo de usuario

--- Reservas_Labs/src/Controladores/Cont_login.java
+++ Reservas_Labs/src/Controladores/Cont_login.java
@@ -3,11 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controladores;
+import Modelos.Usuario;
+import Utilidades.Recurso;
 import Vista.Login;
+import java.util.ArrayList;
 /**
  *
  * @author William
  */
 public class Cont_login {
+    ArrayList<Usuario> usuario = new ArrayList<>();
+    Login vista_login = new Login();
+    Recurso rec = new Recurso();
     
 }
--- Reservas_Labs/src/Modelos/Usuario.java
+++ Reservas_Labs/src/Modelos/Usuario.java
@@ -19,13 +19,33 @@ import java.sql.SQLException;
  */
 public class Usuario {
     private int id_usuario;
-    private String nombre_usuario,contraseña;
+    private String nombre_usuario,contraseña,pregunta_seguridad,respuesta;
 
-    public Usuario(int id_usuario, String nombre_usuario, String contraseña) {
+    public Usuario(int id_usuario, String nombre_usuario, String contraseña, String pregunta_seguridad, String respuesta) {
         this.id_usuario = id_usuario;
         this.nombre_usuario = nombre_usuario;
         this.contraseña = contraseña;
+        this.pregunta_seguridad = pregunta_seguridad;
+        this.respuesta = respuesta;
     }
+
+    public String getPregunta_seguridad() {
+        return this.pregunta_seguridad;
+    }
+
+    public void setPregunta_seguridad(String pregunta_seguridad) {
+        this.pregunta_seguridad = pregunta_seguridad;
+    }
+
+    public String getRespuesta() {
+        return this.respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    
 
     public int getId_usuario() {
         return this.id_usuario;
@@ -61,21 +81,68 @@ public class Usuario {
      }else{
         Statement st = con.createStatement();
         ResultSet rs = null;
-        String consulta = "SELECT usuario.*,permisos.* FROM usuario JOIN permisos ON usuario.permisosID=permisos.id_user;";
+        String consulta = "SELECT id_usuario_PK,nombre_usuario,contraseña,respuesta, descripcion_pregunta FROM Usuarios"
+                                + " JOIN Preguntas_Seguridad ON Usuarios.ID_pregunta=Preguntas_Seguridad.id_pregunta;";
         rs = st.executeQuery(consulta);
         while (rs.next()) {
             try {
-                Usuario user = new Usuario(rs.getInt("id_usuario"),
+                Usuario user = new Usuario(rs.getInt("id_usuario_PK"),
                                                           rs.getString("nombre_usuario") ,
-                                                          rs.getString("contraseña"));
+                                                          rs.getString("contraseña"),
+                                                          rs.getString("descripcion_pregunta"),
+                                                          rs.getString("respuesta"));
                 usuario.add(user);
             } catch (Exception ex) {
                 Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         st.close();
-        rs.close();}
+        rs.close();
+     }
     return usuario;
     }
     
+    public void RegistroUsuarios(String nombre_usuario, String contraseña, int pregunta_seguridad, String respuesta)throws SQLException{
+    Conexion conexion = new Conexion();   
+    Recurso rec = new Recurso();
+    Connection con=conexion.Conectar();
+     if (con==null) {
+             rec.aviso("No tiene conexion RECUERDE cada accion que realize en el programa no se va a guardar");
+     }else{
+        Statement st = con.createStatement();
+        String consulta = "INSERT INTO Usuarios(nombre_usuario,contraseña,respuesta,id_pregunta)"
+                                     + "VALUES('"+nombre_usuario+"','"+contraseña+"','"+respuesta+"',"+pregunta_seguridad+");";
+        st.executeUpdate(consulta);
+        st.close();
+        con.close();
+     }
+     
+    }
+    
+    public ArrayList<PreguntasS> DataPreguntas() throws SQLException{
+        ArrayList<PreguntasS> preguntas = new ArrayList<>();
+        Conexion conexion = new Conexion();   
+        Recurso rec = new Recurso();
+        Connection con=conexion.Conectar();
+        if (con==null) {
+             rec.aviso("No tiene conexion RECUERDE cada accion que realize en el programa no se va a guardar");
+        }else{
+            Statement st = con.createStatement();
+            ResultSet rs = null;
+            String consulta = "SELECT * FROM Preguntas_Seguridad;";
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                try {
+                    PreguntasS pregunta = new PreguntasS(rs.getInt("id_pregunta"),
+                                                          rs.getString("descipcion_pregunta"));
+                    preguntas.add(pregunta);
+                } catch (Exception ex) {
+                    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        st.close();
+        rs.close();
+     }
+        return preguntas;
+    }
 }
