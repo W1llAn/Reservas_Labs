8b86cff742058cd911487b0f07998fcb470ba731
TicheKiwar <kiwartiche@gmail.com>
2024-05-19 17:52:42 -0500

    Crear Metodos para el dto de blocks

--- Reservas_Labs/src/Modelos/BlockDB.java
+++ Reservas_Labs/src/Modelos/BlockDB.java
@@ -4,10 +4,38 @@
  */
 package Modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Kiwar
  */
 public class BlockDB {
-    
+   
+    private Conexion con;
+
+    public BlockDB() {
+        con = new Conexion();
+    }
+ public List<Block> blockList() {
+        List<Block> blocks = new ArrayList<>();
+        try (Connection cn = con.Conectar(); PreparedStatement preparedStatement = cn.prepareStatement(
+                "SELECT * FROM bloques"
+        )) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Block block = new Block(resultSet.getInt("id"), resultSet.getString("nombre_bloque"));
+                blocks.add(block);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return blocks;
+    }
+
 }
