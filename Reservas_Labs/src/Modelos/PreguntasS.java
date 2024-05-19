ec03f93aa1db65e4a0d7eea7958d204fb7dfc361
W1llAn <wchimborazo2840@uta.edu.ec>
2024-05-19 00:26:36 -0500

    Se cambio de lugar el metodo encargado del consultar las preguntas en la base de datos

--- Reservas_Labs/src/Modelos/PreguntasS.java
+++ Reservas_Labs/src/Modelos/PreguntasS.java
@@ -4,6 +4,15 @@
  */
 package Modelos;
 
+import Utilidades.Recurso;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author William
@@ -31,6 +40,33 @@ class PreguntasS {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+    
+        public ArrayList<PreguntasS> DataPreguntas() throws SQLException{
+        ArrayList<PreguntasS> preguntas = new ArrayList<>();
+        Conexion conexion = new Conexion();   
+        Recurso rec = new Recurso();
+        Connection con=conexion.Conectar();
+        if (con==null) {
+             rec.aviso("No tiene conexion RECUERDE cada accion que realize en el programa no se va a guardar");
+        }else{
+            Statement st = con.createStatement();
+            ResultSet rs = null;
+            String consulta = "SELECT * FROM Preguntas_Seguridad;";
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                try {
+                    PreguntasS pregunta = new PreguntasS(rs.getInt("id_pregunta"),
+                                                          rs.getString("descipcion_pregunta"));
+                    preguntas.add(pregunta);
+                } catch (Exception ex) {
+                    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        st.close();
+        rs.close();
+     }
+        return preguntas;
     }
     
 }
