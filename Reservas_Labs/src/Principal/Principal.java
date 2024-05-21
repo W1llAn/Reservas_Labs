dab1fc2c69a09a980d2bad48fe85005374210e60
W1llAn <wchimborazo2840@uta.edu.ec>
2024-05-21 07:59:50 -0500

    arreglo de la clase principal

--- Reservas_Labs/src/Principal/Principal.java
+++ Reservas_Labs/src/Principal/Principal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,56 +6,17 @@ package Principal;
 import Controladores.Cont_login;
 import Modelos.Usuario;
 import Vista.Login;
+import java.sql.SQLException;
 
 /**
  *
  * @author William
  */
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Login log = new Login();
         Usuario user = new Usuario();
         Cont_login controlador = new Cont_login(log, user);
         log.setVisible(true);
     }
 }
-
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Principal;
-
-import Controladores.guardarReserva;
-import Modelos.Conexion;
-import Modelos.Reserva;
-import Vista.Reservas;
-import javax.swing.JFrame;
-
-/**
- *
- * @author William
- */
-public class Principal {
-
-    public static void main(String[] args) {
-        try {
-            new Conexion().Conectar();
-            Reservas visualDeReserva = new Reservas();
-            Reserva mres= new Reserva();
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(930, 600);  // Ajusta el tamaño según necesites
-            frame.add(visualDeReserva);  // 'res' es tu JInternalFrame
-            frame.setVisible(true);
-            frame.setLocationRelativeTo(null);
-            guardarReserva gr = new guardarReserva(visualDeReserva,mres);
-            visualDeReserva.setVisible(true);
-        } catch (Exception e) {
-            e.printStackTrace();  // Esto mostrará el error en la consola
-        }
-
-    }
-}
->>>>>>> develop
