73db4b51575ee6c651aee3074c57c50bf0301b92
Luis <lquishpe5771@uta.edu.ec>
2024-06-08 22:49:36 -0500

    Se cambio la interfaz de reservas ademas se crearon metodos que comprenden a la parte de busqueda por cedula

--- Reservas_Labs/src/Modelos/Horario.java
+++ Reservas_Labs/src/Modelos/Horario.java
@@ -4,13 +4,20 @@
  */
 package Modelos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 /**
  *
  * @author William
  */
 public class Horario {
-    private String fecha_dia, hora_inicio,hora_final, nombre_dia ;
+    private String fecha_dia, hora_inicio,hora_final, nombre_dia,materia,descripcion;
     private boolean esReserva;
+    private int id_laboratorio,id_responsable;
+    
 
     public Horario(String fecha_dia, String hora_inicio, String hora_final, String nombre_dia, boolean esReserva) {
         this.fecha_dia = fecha_dia;
@@ -20,7 +27,51 @@ public class Horario {
         this.esReserva = esReserva;
     }
 
+    public Horario(String fecha_dia, String hora_inicio, String hora_final, String nombre_dia, String materia, int id_laboratorio, int id_responsable,String descripcion) {
+        this.fecha_dia = fecha_dia;
+        this.hora_inicio = hora_inicio;
+        this.hora_final = hora_final;
+        this.nombre_dia = nombre_dia;
+        this.materia = materia;
+        this.id_laboratorio = id_laboratorio;
+        this.id_responsable = id_responsable;
+        this.descripcion= descripcion;
+    }
+    
+
     public Horario() {
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getMateria() {
+        return this.materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getId_laboratorio() {
+        return this.id_laboratorio;
+    }
+
+    public void setId_laboratorio(int id_laboratorio) {
+        this.id_laboratorio = id_laboratorio;
+    }
+
+    public int getId_responsable() {
+        return this.id_responsable;
+    }
+
+    public void setId_responsable(int id_responsable) {
+        this.id_responsable = id_responsable;
     }
     
     
@@ -65,5 +116,27 @@ public class Horario {
         this.esReserva = esReserva;
     }
     
-    
+      public String decifrarDia(String fecha) {
+          // Formato de la fecha de entrada
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Parsear la fecha
+        LocalDate fechaLocalDate = LocalDate.parse(fecha, formatter);
+
+        // Obtener el día de la semana
+        String diaDeLaSemana = fechaLocalDate.getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("es", "ES"));
+
+       System.out.println("El dia de la fecha:"+fecha.toString()+"es:"+diaDeLaSemana);
+        return diaDeLaSemana;
+        
+    }
+      
+      private boolean busquedaCedula(String cedula) {
+          for (int i = 0; i < Almacen.getInstance().listResponsables.size(); i++) {
+              if (Almacen.getInstance().listResponsables.get(i).getCedula().equals(cedula)) {
+                  return true;
+              }
+          }
+        return false;
+    }
 }
